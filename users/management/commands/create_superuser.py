d9fd145e599e2691d8a06878621d614916adcd75
Dr.T. <Aohudyakov@gmail.com>
2025-08-28 15:46:50 +0500

    вроде все настроил, сделал приложение юзер

---
+++ users/management/commands/create_superuser.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+
+from ...models import User
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create_superuser(
+            username=os.getenv("ADMIN_USERNAME"), email=os.getenv("ADMIN_EMAIL")
+        )
+
+        user.set_password(os.getenv("ADMIN_PASSWORD"))
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
