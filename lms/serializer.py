8ee3144326729bd13c1536289d3ed4bd86d94c58
Dr.T. <Aohudyakov@gmail.com>
2025-09-07 21:42:28 +0500

    сделал валидатор для проверки ссылки на видео

--- lms/serializer.py
+++ lms/serializer.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
-
 from .models import Course, Lesson
-
+from .validators import YouTubeLinkValidator
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
-
+        validators = [
+            YouTubeLinkValidator(fields=['video_link', 'description'])
+        ]
 
 class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.IntegerField(read_only=True)  # ← serializers.IntegerField!
+    lessons_count = serializers.IntegerField(read_only=True)
     lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
 
     class Meta:
         model = Course
         fields = ["id", "name", "description", "preview", "lessons_count", "lessons", "owner"]
+        validators = [
+            YouTubeLinkValidator(fields=['description'])
+        ]
